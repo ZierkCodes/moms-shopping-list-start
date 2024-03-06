5a5decb58b1c36867b93bcc96a4b678979e3282b
Gage Zierk <zierk.gage@gmail.com>
2024-03-06 16:10:06 -0600

    added edit item functionality
    clear

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 // TODO: Grab the list
-const shoppingList = document.getElementById("list");
+const shoppingList = document.getElementById("list")
 // TODO: Grab the input field
 const titleInput = document.getElementById("title")
 // TODO: Grab the buttons.
 const submitButton = document.getElementById("submit-item")
 // const editButtons = document.querySelectorAll(".edit-button")
-// const delButtons = document.querySelectorAll(".delete-button")
+const delButtons = document.querySelectorAll(".delete-button")
 
 // TODO: Add click event to the button.
-submitButton.addEventListener('click', () => {
-
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    addItem(titleInput.value)
+    titleInput.value = ""
 });
+
 // TODO: Create a function to add item.
     // ! Requirement: Must have:
     // !   - item text
@@ -22,20 +25,36 @@ submitButton.addEventListener('click', () => {
     // TODO: Edit button must get pass the Parent node.
 
 function addItem(title) {
+    console.log("Running addItem")
     let newItem = document.createElement("li")
     newItem.classList.add("item")
+
     let newTitle = document.createElement("div")
     newTitle.textContent = title
+
     let editBtn = document.createElement("button")
     editBtn.classList.add("edit-button")
-    editBtn.value = "edit"
+    editBtn.innerText = "edit"
+
     let delBtn = document.createElement("button")
     delBtn.classList.add("delete-button")
-    delBtn.value = "X"
+    delBtn.innerText = "X"
+
     editBtn.addEventListener('click', (e) => {
-        
+        console.log("Pressed the edit button", e)
+        editItem(e.target.parentElement)
     });
-}
+
+    newItem.appendChild(newTitle)
+    newItem.appendChild(editBtn)
+    newItem.appendChild(delBtn)
+    shoppingList.appendChild(newItem)
+};
+
+delButtons.addEventListener('click', (e) => {
+    console.log("Pressed the delete button")
+    deleteItem(e.target.parentNode);
+});
 
 
 // TODO: Create a function to edit item.
@@ -48,18 +67,68 @@ function addItem(title) {
     // TODO: Add the save button.
 
 function editItem(listItem) {
-    listItem.removeChild("button")
-    
+    let title;
+    listItem.childNodes.forEach(element => {
+        console.log(element.tagName)
+        if(element.tagName === "DIV") {
+            title = element.innerText
+        }
+    });
+
+    listItem.innerHTML = ""
+
+    let editInput = document.createElement("input")
+    editInput.value = title
+
+    let saveBtn = document.createElement("button")
+    saveBtn.classList.add("save-button")
+    saveBtn.innerText = "save"
+
+    listItem.append(editInput, saveBtn)
+
+    saveBtn.addEventListener('click', (e) => {
+        saveItem(e.target.parentNode)
+    });
 }
 
 // TODO: Create a function to save item.
-    // ! Requirement: Parent Node (Element) paramter
+    // ! Requirement: Parent Node (Element) parameter
     // TODO: Get the value of the input field and assign to variable
     // TODO: Remove the input and save button.
     // TODO: Create the div, edit, and delete buttons.
     // TODO: Set the div's text content to the variable with the input's value.
     // TODO: Add all those elements to the parent element.
 
+function saveItem(listItem) {
+    let titleEdit;
+
+    listItem.childNodes.forEach(element => {
+        if(element.tagName === "INPUT") {
+            titleEdit = element.value
+        }
+    });
+
+    listItem.innerHTML = "";
+
+    let changedTitle = document.createElement("div");
+    changedTitle.textContent = titleEdit;
+
+    let editBtn = document.createElement("button");
+    editBtn.classList.add("edit-button");
+    editBtn.innerText = "edit";
+
+    let delBtn = document.createElement("button");
+    delBtn.classList.add("delete-button");
+    delBtn.innerText = "X";
+
+    listItem.appendChild(changedTitle);
+    listItem.appendChild(editBtn);
+    listItem.appendChild(delBtn);
+}
+
 // TODO: Create a function to delete item.
-    // ! Requirement: Parent Node (Element) paramter
-    // I believe you can figure this one out yourself. <3 :) 
+    // ! Requirement: Parent Node (Element) parameter
+
+function deleteItem (listItem) {
+    listItem.innerHTML = "";
+};
